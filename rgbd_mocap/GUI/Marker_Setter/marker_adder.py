7a6054eaf382da34d97d2334016ce4a794953884
Kael Facon <kael.facon@epita.fr>
2024-01-11 11:07:00 -0500

    Add: GUI Initial commit

---
+++ rgbd_mocap/GUI/Marker_Setter/marker_adder.py
@@ -0,0 +1,221 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from Marker_Setter.marker_list import MarkerList
+
+
+class AddMarkerEntry(QLineEdit):
+    """
+    Text entry to write the name of the DragMarker to be added.
+    Return Key is linked to the AddMarkerButton.
+    """
+    def __init__(self, add_marker_button=None, default_text='New maker'):
+        """
+        Create a AddMarkerEntry to link with an AddMarkerButton
+        :param add_marker_button: The AddMarkerButton to be linked with
+        :type add_marker_button: AddMarkerButton
+        :param default_text: Default text to be displayed in the entry
+        :type default_text: str
+        """
+        super(AddMarkerEntry, self).__init__(default_text)
+        self.default_text = default_text
+
+        if add_marker_button is not None:
+            self.returnPressed.connect(lambda: add_marker_button.add_marker() or self.reset())
+
+    def reset(self):
+        """
+        Reset the entry text.
+        :return: None
+        """
+        self.clear()
+
+    def focusInEvent(self, a0, QFocusEvent=None):
+        """
+        When focused clear the default text.
+        :param a0:
+        :type a0:
+        :param QFocusEvent:
+        :type QFocusEvent:
+        :return: None
+        """
+        self.clear()
+
+    def focusOutEvent(self, a0, QFocusEvent=None):
+        """
+        When unfocused set back the default text.
+        :param a0:
+        :type a0:
+        :param QFocusEvent:
+        :type QFocusEvent:
+        :return: None
+        """
+        self.setText(self.default_text)
+
+
+class AddMarkerButton(QWidget):
+    """
+    QPushButton linked to an AddMarkerEntry and contained in
+    a MarkerAdder. Pressing the button will add a DragMarker
+    in the MarkerList with the name written in the entry.
+    """
+    def __init__(self, marker_list: MarkerList, parent=None):
+        super(AddMarkerButton, self).__init__(parent)
+        self.marker_list = marker_list
+        self.entry = AddMarkerEntry(self)
+
+        self.add_button = QPushButton('Add')
+        self.add_button.pressed.connect(self.add_marker)
+
+        ### Set layout
+        layout = QHBoxLayout()
+        layout.addWidget(self.add_button)
+        layout.addWidget(self.entry)
+
+        self.setLayout(layout)
+
+    def add_marker(self):
+        """
+        Add a DragMarker in the MarkerList with
+        the name written in the entry.
+        :return: None
+        """
+        ### Get the text in the entry
+        name = self.entry.text()
+
+        ### If the name isn't valid return
+        if name is None or name == "":
+            return
+
+        # self.entry.reset()
+        self.marker_list.add_marker(name)
+
+
+class LoadAndRemoveMarkerButtons(QWidget):
+    """
+    A set of two buttons linked with a MarkerList.
+    The left button remove the currently selected DragMarker
+    of the MarkerList.
+    The right button remove all the DragMarker contained in
+    the MarkerList.
+    """
+    def __init__(self, marker_list: MarkerList, parent=None):
+        """
+        Initialize the RemoveMarkerButtons and link them to
+        the given MarkerList
+        :param marker_list: MarkerList to link
+        :type marker_list: MarkerList
+        :param parent: MarkerSetter
+        :type parent: MarkerSetter
+        """
+        super(LoadAndRemoveMarkerButtons, self).__init__(parent)
+        self.load_button = QPushButton('Load Marker Set')
+        self.load_button.pressed.connect(marker_list.load_marker_set)
+
+        self.remove_button = QPushButton('Remove')
+        self.remove_button.pressed.connect(marker_list.remove_marker)
+
+        self.remove_all_button = QPushButton('Remove All')
+        self.remove_all_button.pressed.connect(marker_list.remove_all_marker)
+
+        ### Set Layout
+        layout = QGridLayout()
+        layout.addWidget(self.load_button, 0, 0, 1, 2)
+        layout.addWidget(self.remove_button, 1, 0, 1, 1)
+        layout.addWidget(self.remove_all_button, 1, 1, 1, 1)
+        self.setLayout(layout)
+
+
+class HorizontalSeparator(QFrame):
+    def __init__(self, width=1):
+        """
+        Create a horizontal separator.
+        :param width: thickness of the separator (by default 1)
+        :type width: int
+        """
+        super(HorizontalSeparator, self).__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.setLineWidth(width)
+
+
+class MarkerAdder(QWidget):
+    """
+    A QWidget object containing a MarkerList, a
+    AddMarkerButton at the top and a RemoveMarkerButtons
+    at the bottom.
+    """
+    def __init__(self, l=[], parent=None):
+        """
+        Initialize a MarkerAdder widget.
+        :param l: List of string containing the names of the DragMarker to be inited in the MarkerList.
+        :type l: list[str]
+        :param parent: Container parent
+        :type parent: QObject
+        """
+        super(MarkerAdder, self).__init__(parent)
+        self.setAcceptDrops(True)
+        self.setMouseTracking(True)
+
+        self.list_marker = MarkerList(l, self)
+        ### Create the layout with the AddMarkerButton on the top
+        layout = QVBoxLayout(self)
+        self.add_marker_widget = AddMarkerButton(self.list_marker, self)
+        layout.addWidget(self.add_marker_widget)
+
+        ### Separator between AddMarkerButton and the MarkerList
+        layout.addWidget(HorizontalSeparator())
+
+        ### Also creating a scrolling area for the MarkerList
+        scroll = QScrollArea()
+        scroll.setWidget(self.list_marker)
+        scroll.setWidgetResizable(True)
+        layout.addWidget(scroll)
+
+        ### At the bottom of the widget the RemoveMarkerButtons
+        self.remove_marker_widget = LoadAndRemoveMarkerButtons(self.list_marker)
+        layout.addWidget(self.remove_marker_widget)
+
+        self.setLayout(layout)
+
+    def dragEnterEvent(self, e):
+        """
+        Accept drag enter event
+        :param e: Drag event
+        :type e: QDragEnterEvent
+        :return: None
+        """
+        e.accept()
+
+    def dropEvent(self, e):
+        """
+        Pass the drop event to the MarkerList
+        :param e: Drop event
+        :type e: QDropEvent
+        :return: None
+        """
+        self.list_marker.dropEvent(e)
+
+    def markers_to_dict(self):
+        markers = []
+        for marker in self.list_marker:
+            markers.append(marker.text())
+
+        return markers
+
+    def load_markers(self, markers):
+        for marker in markers:
+            self.list_marker.add_marker(marker)
+
+    def clear(self):
+        self.list_marker.remove_all_marker()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    list = ['Acrom', 'Scapula', 'Arm_l', ]
+    w = MarkerAdder(list)
+    w.show()
+
+    app.exec_()
---
+++ rgbd_mocap/GUI/main_window.py
@@ -0,0 +1,262 @@
+import csv
+import json
+import os
+import re
+import sys
+
+import cv2
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+from Marker_Setter.marker_setter_tab import MarkerSetter
+from video_crop_widget import CropWidget
+from video_crop_window import CropWindow
+from Utils.video_player import VideoControl
+from Video_cropping.crop_video_tab import CropVideoTab
+from Video_cropping.crop_video import VideoCropper
+from Utils.error_popup import ErrorPopUp
+from Utils.warning_popup import WarningPopUp
+from Utils.file_dialog import SaveDialog, LoadDialog, LoadFolderDialog
+
+
+class MainWindowMenuBar(QMenuBar):
+    """
+    Menu bar for the Crop window.
+    Contains shortcut for loading
+    folder of image, crop saved files
+    and for saving actual crops.
+    """
+
+    def __init__(self, main_window):
+        """
+        Initialize a CropWindowMeuBar and link it
+        to a CropWindow. The actions contained in
+        the CropWindowMenuBar will be linked to the
+        given CropWindow.
+        :param crop_window: CropWindow to link with
+        :type crop_window: CropWindow
+        """
+        super(MainWindowMenuBar, self).__init__(main_window)
+        self.mw = main_window
+
+        ### Init menu bar actions
+        self.open_file_action = QAction('Load Directory', self)
+        self.open_file_action.triggered.connect(main_window.vce.load_folder)
+
+        self.quit_app_action = QAction('Quit', self)
+        self.quit_app_action.triggered.connect(main_window.close)
+
+        # self.save_crops_action = QAction('Save Crops', self)
+        # self.save_crops_action.triggered.connect(main_window.save_crops)
+
+        self.load_crops_action = QAction('Load Crops', self)
+        self.load_crops_action.triggered.connect(main_window.load_crop)
+
+        self.save_project_action = QAction('Save Project', self)
+        self.save_project_action.triggered.connect(main_window.save_project)
+
+        self.load_project_action = QAction('Load Project', self)
+        self.load_project_action.triggered.connect(main_window.load_project)
+
+        ### Init and fill menu bar
+        file = self.addMenu('File')
+        file.addAction(self.open_file_action)
+        file.addSeparator()
+        # file.addAction(self.save_crops_action)
+        file.addAction(self.load_crops_action)
+        file.addSeparator()
+        file.addAction(self.save_project_action)
+        file.addAction(self.load_project_action)
+        file.addSeparator()
+        file.addAction(self.quit_app_action)
+
+
+class SwitchWindowButton(QPushButton):
+    def __init__(self, crop_window):
+        super(SwitchWindowButton, self).__init__(crop_window)
+
+
+class MainWindowButton(QWidget):
+    def __init__(self, main_window):
+        super(MainWindowButton, self).__init__(main_window)
+        layout = QHBoxLayout()
+
+        ## Buttons (Save/Quit)
+        self.save_crops_button = QPushButton("Save")
+        self.save_crops_button.clicked.connect(main_window.quick_save)
+        self.save_crops_button.setMaximumWidth(100)
+
+        self.quit_app_button = QPushButton("Quit")
+        self.quit_app_button.clicked.connect(main_window.close)
+        self.quit_app_button.setMaximumWidth(100)
+
+        self.marker_set_button = QPushButton('Set Markers')
+        self.marker_set_button.clicked.connect(main_window.marker_setting)
+        self.marker_set_button.setMaximumWidth(100)
+
+        layout.addWidget(self.marker_set_button, 1, Qt.AlignLeft)
+        layout.addWidget(self.save_crops_button, 0, Qt.AlignRight)
+        layout.addWidget(self.quit_app_button, 0, Qt.AlignRight)
+
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    """
+    A CropWindow containing a VideoTab and VideoPlayer.
+    The user can load a folder of images, then create crops
+    and edit the video with various parameters.
+
+    """
+
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setGeometry(300, 200, 900, 600)
+        self.setWindowTitle("RGBD MoCap")
+        self.layout = QGridLayout()
+
+        ### Video cropping and editing
+        self.vce = CropWidget(self)
+
+        ### Menu bar
+        self.menu_bar = MainWindowMenuBar(self)
+        self.setMenuBar(self.menu_bar)
+
+        ### Marker Setter
+        self.marker_setter_tab = None
+
+        ### Project informations
+        self.project_dict = {}
+
+        ### Buttons
+        self.buttons = MainWindowButton(self)
+
+        ### Place in layout
+        self.layout.addWidget(self.vce, 0, 0, 1, 1,)
+        self.layout.addWidget(self.buttons, 1, 0, 1, 1, Qt.AlignBottom)
+
+        self.central_video_widget = QWidget()
+        self.central_video_widget.setLayout(self.layout)
+        self.setCentralWidget(self.central_video_widget)
+
+    ### Saves and Loads
+    def quick_save(self):
+        pass
+        # if self.save_file is None:
+        #     self.save_project()
+        #
+        # else:
+        #     self.save_project_file(self.save_file)
+
+    def load_crop(self):
+        LoadDialog(parent=self,
+                   caption='Load crop file',
+                   filter='Save File (*.json);; Any(*)',
+                   load_method=self.vce.video_cropper.load_crops_file)
+
+    def save_project(self):
+        SaveDialog(parent=self,
+                   caption='Save Project',
+                   filter='Save File (*json)',
+                   suffix='json',
+                   save_method=self.save_project_file)
+
+    def save_project_file(self, file):
+        self.save_file = file
+
+        ### Write it
+        with open(file, 'w') as file:
+            json.dump(self.project_to_dict(), file, indent=2)
+
+    def project_to_dict(self):
+        parameters_vce = self.vce.to_dict()
+        parameters_ms = self.marker_setter_tab.to_dict()
+
+        for crop_ms in parameters_ms['crops']:
+            for crop_vce in parameters_vce['crops']:
+                if crop_vce['name'] == crop_ms['name'] and crop_vce['markers'] == []:
+                    crop_vce['markers'] = crop_ms['markers']
+                    break
+            ### Way to unplace markers if crop not found rather than removing them
+
+        parameters_vce['markers'] = parameters_ms['markers']
+        return parameters_vce
+
+    def save_crops(self):
+        crops = []
+        for tab in self.video_tab.tabs[1:]:
+            tab_dict = tab.save_dic()
+
+            # If marker_setter is placed then
+            if self.marker_setter_tab is not None:
+                tab_dict['markers'] = self.marker_setter_tab.get_markers_from(tab.name)
+
+            crops.append(tab_dict)
+
+        return crops
+
+    def load_project(self):
+        LoadDialog(parent=self,
+                   caption='Load project file',
+                   filter='Save File (*.json);; Any(*)',
+                   load_method=self.load_project_file)
+
+    def load_project_file(self, file):
+        with open(file, 'r') as f:
+            parameters = json.load(f)
+            try:
+                self.vce.load_project_dict(parameters)
+
+                if self.marker_setter_tab is None:
+                    self.create_marker_setter()
+
+                self.marker_setter_tab.load_project_dict(parameters)
+
+                self.set_vce()
+
+            except TypeError and KeyError:
+                ErrorPopUp('File could not be loaded, wrong format', self)
+
+    def set_vce(self):
+        if self.marker_setter_tab is not None:
+            self.marker_setter_tab.setParent(None)
+            self.layout.addWidget(self.vce, 0, 0, 1, 1)
+
+    def set_marker_setter(self):
+        if self.marker_setter_tab is not None:
+            self.vce.setParent(None)
+            self.layout.addWidget(self.marker_setter_tab, 0, 0, 1, 1)
+
+    def create_marker_setter(self):
+        path = self.vce.get_first_frame()
+        if path is None:
+            ErrorPopUp('Cannot find first frame index')
+            return
+
+        crops = self.vce.get_crops()
+        self.marker_setter_tab = MarkerSetter(path=path,
+                                              marker_set=[],
+                                              crops=crops, )
+
+    def marker_setting(self):
+        ### Warning Pop Up
+        if not WarningPopUp('Pass to marker settings').res:
+            return
+
+        ### If no MarkerSetter then create a new MarkerSetter
+        ### Else a MarkerSetter as already been loaded then reload it to apply eventual changes
+        if self.marker_setter_tab is None:
+            self.create_marker_setter()
+        else:
+            self.marker_setter_tab.reload(self.project_to_dict())
+
+        self.set_marker_setter()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = MainWindow()
+    # demo.vce.dir = "../../../../rgbd_mocap/data_files/P4_session2/gear_20_15-08-2023_10_52_14/"
+    # demo.vce.load_images()
+    demo.show()
+    sys.exit(app.exec_())
